fd5ffc948669523bd263c6566f33429b4c000795
zhangchuanliang <zhangchuanliang@yiche.com>
2017-06-20 12:33:51 +0800

    添加webpack-dev-server配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 var webpack = require('webpack'),
-    path = require('path');
+  path = require('path')
 module.exports = {
-    entry: './script/drag.js',
-    output: {
-        path: path.join(__dirname, './script'),
-        filename: 'drag.bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env']
-                },
-                exclude: /(node_modules)/
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8090',
+    './script/drag.js'
+  ],
+  output: {
+    path: path.join(__dirname, './script'),
+    filename: 'drag.bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        },
+        exclude: /(node_modules)/
+      }
     ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
 }
